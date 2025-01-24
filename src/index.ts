2b69483f226b3b76787972d1245b7bf42d185890
ekene leonard <nwobodoe71@gmail.com>
2025-01-24 17:41:28 +0100

    add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { app } from './app';
-require('dotenv').config();
+import { app } from "./app";
+require("dotenv").config();
+import { connectToDB } from "./config/db";
+import { DB_CONNECTION_STRING } from "./constants";
+
+console.log("DB_CONNECTION_STRING", DB_CONNECTION_STRING);
 
 //Create Server
-app.listen(process.env.PORT, () => { 
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT, async () => {
+  await connectToDB(DB_CONNECTION_STRING);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
