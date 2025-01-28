8e3fda2626cd258146f8c74efa71c624bb29d635
unknown <daleksklee@gmail.com>
2025-01-28 15:30:24 +0100

    add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import { app } from './app';
-import dotenv from 'dotenv';
-import { connectToDB } from './config/db';
-import { DB_CONNECTION_STRING } from './constants';
+import { app } from "./app";
+import dotenv from "dotenv";
+import { connectToDB } from "./config/db";
+import { DB_CONNECTION_STRING } from "./constants";
+import logger from "./utils/logger.utils";
+import { Request, Response, NextFunction } from "express";
 
 dotenv.config();
-console.log('DB_CONNECTION_STRING', DB_CONNECTION_STRING);
+console.log("DB_CONNECTION_STRING", DB_CONNECTION_STRING);
 
-//Create Server
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.on("finish", () => {
+    const logMessage = `${req.method} ${req.url} ${res.statusCode}`;
+
+    if (Object.keys(req.query).length) {
+      logger.info(`${logMessage} - Query Params: ${JSON.stringify(req.query)}`);
+    }
+
+    if (req.body && Object.keys(req.body).length > 0) {
+      logger.info(`${logMessage} - Request Body: ${JSON.stringify(req.body)}`);
+    } else {
+      logger.info(logMessage);
+    }
+  });
+  next();
+});
+
 app.listen(process.env.PORT, async () => {
   await connectToDB(DB_CONNECTION_STRING);
   console.log(`Server is running on port ${process.env.PORT}`);
