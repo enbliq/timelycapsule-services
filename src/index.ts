c06b74a243da217aa0ef1ed3f6cc9f190b92884e
Fahmed2024 <155979739+Fahmed2024@users.noreply.github.com>
2025-01-29 10:05:47 +0100

    commit not found error  middleware

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import { app } from './app';
 import dotenv from 'dotenv';
 import { connectToDB } from './config/db';
 import { DB_CONNECTION_STRING } from './constants';
+import { notFoundMiddleware } from './model/middleware/notFoundMiddleware';
 
 dotenv.config();
 console.log('DB_CONNECTION_STRING', DB_CONNECTION_STRING);
+
+// 404 middleWare
+app.use(notFoundMiddleware);
 
 //Create Server
 app.listen(process.env.PORT, async () => {
---
+++ src/model/middleware/notFoundMiddleware.ts
@@ -0,0 +1,2 @@
+export const notFoundMiddleware = (req, res) =>
+  res.status(404).send('Not Found');
