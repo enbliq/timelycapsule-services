7826988c4c733958e2538a31d979204639bfbcee
benard <benardsimon7@gmail.com>
2025-01-31 14:28:18 +0100

    add register endpoint

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -3,8 +3,8 @@ import { Auth } from "../controllers/auth.controller";
 
 const authRouter = (router: express.Router) => {
   router.post("/login", Auth.login);
-  router.post('/resendemail', Auth.resendVerificationEmail);
-
+  router.post("/resendemail", Auth.resendVerificationEmail);
+  router.post("/register", Auth.register);
 };
 
 export default authRouter;
