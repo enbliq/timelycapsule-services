da12d9ca9bd8a2599570a5a5c9450637c42933b0
phertyameen <aminubabafatima8@gmail.com>
2025-02-02 02:06:54 +0100

    added getAllUsers in controller and specified the routes

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -5,6 +5,7 @@ import auth from '../middleware/auth';
 const authRouter = (router: express.Router) => {
   router.get('/users/:id', UserController.getUserById);
   router.get('/user', auth, UserController.getCurrentUser);
+  router.get('/users', auth, UserController.getAllUsers);
 };
 
 export default authRouter;
